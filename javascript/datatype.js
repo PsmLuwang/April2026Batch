35e7d09eb346ffcb86fa4bed88f16d8b8196551a
Priyansu <spriyansumeitei@gmail.com>
2026-05-11 20:54:43 +0530

    add initial HTML and JavaScript files for primitive data types demonstration

---
+++ javascript/datatype.js
@@ -0,0 +1,30 @@
+
+/*
+
+// PRIMITIVE DATATYPES
+Number
+String
+Boolean
+
+Null
+undefined
+*/
+
+// let, const , var
+// const numberBox = 5; // NUMBER
+
+// const textBox = "text"; // STRING
+
+// const isOkay = true; // BOOLEAN
+
+const number_box_is_here = 10;
+const numberBoxIsHere = 20;
+
+const abc = null;
+const def = undefined;
+
+console.log(abc);
+console.log(def);
+
+
+
