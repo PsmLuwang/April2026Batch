f07c93cdcdd401454d7c8356b4440aa0deb83b6b
Priyansu <spriyansumeitei@gmail.com>
2026-05-13 21:21:15 +0530

    add if-else JavaScript logic and update HTML title and script reference

---
+++ javascript/ifElse.js
@@ -0,0 +1,26 @@
+const a = 998;
+
+// < , > , == , === , >= , <= , !=
+
+if (a % 2 == 0) {
+  console.log("even");
+  const c = a + 2;
+  console.log(c);
+  
+} 
+else {
+  console.log("odd");
+}
+
+
+// 1. take two numbers (a and b). 
+// if they are equal then find the sum 
+// otherwise find its difference. 
+
+// 2. take two numbers (a and b). 
+// if the sum is less than 100 then multiply by 3.
+// if the sum is greater than or equal to 100 
+// find the sum is even or odd.
+
+// 3. take two number (a and b) and 
+// find the larger one.
