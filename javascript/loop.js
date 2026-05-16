a16009fd6ddaa3347933d92f0be0ddb03d9a76a5
Priyansu <spriyansumeitei@gmail.com>
2026-05-16 20:33:26 +0530

    add for loop demonstration with loop.js and update index.html

---
+++ javascript/loop.js
@@ -0,0 +1,15 @@
+// for loop
+// a = a + 1 // a += 1 // a++
+
+console.log("Loop started");
+
+for (let i=0; i<5; i++) {
+  if ((i)%2 == 0) {
+    console.log(i, " is even.");
+  }
+  
+}
+
+console.log("Loop ended");
+
+
