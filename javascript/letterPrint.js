c610b8a8c1464bea65b177d99952df1bd7dafd30
Priyansu <spriyansumeitei@gmail.com>
2026-05-19 20:24:35 +0530

    update index.html to reflect nested for loop and add letterPrint.js for demonstration

---
+++ javascript/letterPrint.js
@@ -0,0 +1,42 @@
+
+
+// debugger
+
+for (let i = 1; i <= 7; i++) {
+  let row = "";
+
+  for (let j = 1; j <= 7; j++) {
+
+    if (j==1 || j==7 || i==j) {
+      row += `* `
+    } else {
+      row += "  "
+    }
+  
+  }
+
+  console.log(row);
+  
+}
+
+console.log("");
+
+
+for (let i = 1; i <= 7; i++) {
+  let row = "";
+
+  for (let j = 1; j <= 7; j++) {
+
+    if (j==1 || j==7 || i==j) {
+      row += `* `
+    } else {
+      row += "  "
+    }
+  
+  }
+
+  console.log(row);
+  
+}
+
+// HTML
