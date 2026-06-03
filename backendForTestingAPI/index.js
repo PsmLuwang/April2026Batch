ad1ce6b1738a60ffa84e6b9ed23a4a7bba217baa
Priyansu <spriyansumeitei@gmail.com>
2026-06-03 21:04:33 +0530

    Add student management functionality; implement POST method for new student creation and display logic in HTML

--- backendForTestingAPI/index.js
+++ backendForTestingAPI/index.js
@@ -30,6 +30,27 @@ app.get("/testAPI", (req, res) => {
   }
 })
 
+
+
+// POST Method
+app.post("/newStudent", (req, res) => {
+  try {
+    const student = req.body;
+    
+    console.log(student);
+    
+    
+    res.status(201).json({
+      success: true,
+      message: "Created New Student",
+      student: {...student, id: student.name.slice(0, 4) + "2026"}
+    })
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message || "Invalid Data" })
+  }
+})
+
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
