9d950e597fdaf72d78217c49500c0baf26aec415
Priyansu <spriyansumeitei@gmail.com>
2026-06-10 23:39:25 +0530

    style: enhance NavBar layout with improved item alignment

--- react/frontendLearn/src/components/NavBar.jsx
+++ react/frontendLearn/src/components/NavBar.jsx
@@ -6,10 +6,14 @@ const NavBar = () => {
     <nav className='bg-gray-400 flex justify-between items-center px-5 h-14'>
       <h1 className='font-semibold text-xl'>React</h1>
       
-      <div className='flex gap-5'>
+      <div className='flex items-center gap-5'>
         <Link to={'/'}>Home</Link>
         <Link to={'/contact'}>Contact</Link>
         <Link to={'/registration'}>Registration</Link>
+        <Link to={'/profile'} className='bg-blue-400 text-white font-bold  w-10 h-10  rounded-full  flex items-center justify-center'>
+          M
+        </Link>
+        
       </div>
     </nav>
   )
